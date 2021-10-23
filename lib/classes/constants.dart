29ab0125415a484e3620378a81c2b5b4935baea5
jotalevi <talevineto@gmail.com>
2021-10-23 15:06:27 -0300

    Initial commit for StickLib mobile app

---
+++ lib/classes/constants.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class ColorsLight {
+  final ACCENTBACKDROP = Color.fromARGB(255, 177, 229, 242);
+  final MAINBACKDROP = Color.fromARGB(255, 232, 233, 243);
+  final PRIMARY = Color.fromARGB(255, 54, 65, 62);
+  final SECONDARY = Color.fromARGB(255, 166, 166, 168);
+  final ACCEPT = Color.fromARGB(255, 12, 137, 233);
+  final DECLINE = Color.fromARGB(255, 242, 83, 69);
+}
+
+class ColorsDark {
+  final ACCENTBACKDROP = Color.fromARGB(255, 0, 0, 0);
+  final MAINBACKDROP = Color.fromARGB(255, 61, 61, 61);
+  final PRIMARY = Color.fromARGB(255, 232, 233, 243);
+  final SECONDARY = Color.fromARGB(255, 166, 166, 168);
+  final ACCEPT = Color.fromARGB(255, 12, 137, 233);
+  final DECLINE = Color.fromARGB(255, 183, 31, 31);
+}
